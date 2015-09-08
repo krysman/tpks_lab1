82b86e4bb207d42eda542d31f7bdfb23bdac6a7a
krysman <saprykin8sept@gmail.com>
2015-09-08 11:23:15 +0300

    add InputFileChecker class

---
+++ src/main/java/com/nariki/tpks/lab1/gui/InputFileChecker.java
@@ -0,0 +1,44 @@
+package com.nariki.tpks.lab1.gui;
+
+import java.io.File;
+
+public class InputFileChecker {
+
+    private File fileToCheck;
+    private String errorString;
+
+    public InputFileChecker(File fileToCheck) {
+        this.fileToCheck = fileToCheck;
+    }
+
+    public boolean isFileCorrect() {
+
+        /*
+         *файл корректен если он имеет расширение .txt, он не пустой, и его содержание соответствует матрице инцидентности
+         */
+
+        boolean fileEndsWithTxt = fileToCheck.getPath().endsWith(".txt");
+        if(fileEndsWithTxt) {
+
+            boolean fileIsNotEmpty = 0 < fileToCheck.length();
+            if(fileIsNotEmpty) {
+
+                // TODO: проверка файла на содержание валидной матрицы инцидентности
+
+            } else { // если файл пустой то он не корректный
+                errorString = "Файл пустой!";
+                return false;
+            }
+        } else { // если файл не *.txt то он не корректный
+            errorString = "Расширение файла болжно быть .txt";
+            return false;
+        }
+
+        return false;
+    }
+
+    public String  getErrorMessage() {
+        return errorString;
+    }
+
+}
--- src/main/java/com/nariki/tpks/lab1/gui/MainFrame.java
+++ src/main/java/com/nariki/tpks/lab1/gui/MainFrame.java
@@ -1,5 +1,59 @@
 package com.nariki.tpks.lab1.gui;
 
-public class MainFrame {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
 
+public class MainFrame extends JFrame{
+
+    private boolean fileHasBeenChoosen;
+
+    public MainFrame() {
+        super("ТПКС - Лабораторная работа №1");
+
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(400, 250);
+
+        setResizable(false);
+
+
+
+        setLocationRelativeTo(null); // установить окно по центру экрана
+
+
+        // управляющая  панель
+        JPanel controlPanel = new JPanel();
+        controlPanel.setLayout(new BorderLayout());
+
+        // управляющие кнопки:
+
+        // кнопка выбора файла
+        JButton selectFileButton = new JButton("Выберите файл");
+        selectFileButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                JFileChooser fileChooser = new JFileChooser();
+                int ret = fileChooser.showDialog(null, "Выбрать файл");
+                if(ret == JFileChooser.APPROVE_OPTION) {
+                    File file = fileChooser.getSelectedFile(); // получаем выбраный пользователем файл
+
+                    // отправляем файл на проверку, в зависимости от результата выводим сообщение об ошибке или продолжаем работу
+                    InputFileChecker inputFileChecker = new InputFileChecker(file);
+
+                    if(inputFileChecker.isFileCorrect()) {
+                        fileHasBeenChoosen = true;
+                        // TODO: создание объекта графа по исходным данным из файла
+                    } else {
+                        // с файлом что-то не так определяем проблему и выводим соответствующее сообщение об ошибке
+                        file = null;
+                        fileHasBeenChoosen = false;
+
+                        JOptionPane.showMessageDialog(null, inputFileChecker.getErrorMessage(), "Ошибка!", JOptionPane.ERROR_MESSAGE);
+                    }
+                }
+            }
+        });
+
+    }
 }
