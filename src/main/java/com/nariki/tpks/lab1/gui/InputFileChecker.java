215e8dcbe66019e919027a4a21cdf7666471c16f
krysman <saprykin8sept@gmail.com>
2015-09-10 22:11:26 +0300

    incidence matrix validation

--- src/main/java/com/nariki/tpks/lab1/gui/InputFileChecker.java
+++ src/main/java/com/nariki/tpks/lab1/gui/InputFileChecker.java
@@ -1,6 +1,11 @@
 package com.nariki.tpks.lab1.gui;
 
-import java.io.File;
+import com.nariki.tpks.lab1.model.IncidenceMatrixValidator;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 public class InputFileChecker {
 
@@ -23,7 +28,13 @@ public class InputFileChecker {
             boolean fileIsNotEmpty = 0 < fileToCheck.length();
             if(fileIsNotEmpty) {
 
+                // считать построчно содержание файла
+                String [] matrix = readAllLinesFromFile();
+                IncidenceMatrixValidator incidenceMatrixValidator = new IncidenceMatrixValidator(matrix);
+
                 // TODO: проверка файла на содержание валидной матрицы инцидентности
+
+
                 return true;
             } else { // если файл пустой то он не корректный
                 errorString = "Файл пустой!";
@@ -34,6 +45,22 @@ public class InputFileChecker {
             return false;
         }
 
+    }
+
+    private String[] readAllLinesFromFile() {
+        List<String> allStringsInFile = new LinkedList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(fileToCheck))) {
+            String line;
+            while( (line = br.readLine()) != null) {
+                allStringsInFile.add(line);
+            }
+        } catch(FileNotFoundException e) {
+            e.printStackTrace();
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+
+        return (String[]) allStringsInFile.toArray();
     }
 
     public String  getErrorMessage() {
--- src/main/java/com/nariki/tpks/lab1/model/IncidenceMatrixValidator.java
+++ src/main/java/com/nariki/tpks/lab1/model/IncidenceMatrixValidator.java
@@ -1,12 +1,16 @@
 package com.nariki.tpks.lab1.model;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class IncidenceMatrixValidator {
 
     /*
     *
-    * ???? ?????????????? -> ?????? ??????? ???. ???? ????????? ??????: -1, 0, 1.
-    * ? ?????? ?????? ????? ???? ??? ??????? ??? ??????? ??? 1 ? -1
-    * ???????????? ?????????? ??????(?????) ?????? ?? ??????? ? ??? ???-?? ???(????????) ??????????? ??? = (n*(n-1))/2
+    * строки в матрице могут содержать только: -1, 0, 1, 2.
+    * причем в строке кроме множества 0 может быть либо -1 и 1, либо 2
+    * строк(вершин) должно быть не больше n = 16
+    * столбцев(дуг) должно быть не больше k = (n*(n-1))/2
     *
      */
 
@@ -15,20 +19,77 @@ public class IncidenceMatrixValidator {
 
     private int [][] incidenceMatrix;
     private String [] matrixToCheck;
+    private String errorString;
 
     public IncidenceMatrixValidator(String [] matrixToCheck) {
+
         this.matrixToCheck = matrixToCheck;
 
-        for(String line: matrixToCheck) {
-
-           //TODO: Integer.getInteger()
-        }
     }
 
-    private boolean checkRowAndColumnNumber() {
-       /* if( ! (matrixToCheck.length > MAX_VERTEX_NUMBER)) {
-            return
-        }*/
+    public boolean isIncidenceMatrixValid() {
+        if(isRowNumberOk()) {
+            if(getIntegerFormOfIncidenceMatrix()) {
+                if(isColumnNumberOk()) {
+                    return true;
+                }
+            }
+        }
         return false;
     }
+
+    public String getErrorString() {
+        return errorString;
+    }
+
+    private boolean isRowNumberOk() {
+        if( MAX_VERTEX_NUMBER < matrixToCheck.length) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     *
+     * @return true if matrix is OK, otherwise false. integer form of matrix placed in class field incidenceMatrix
+     */
+    private boolean getIntegerFormOfIncidenceMatrix() {
+
+        // получаем матрицу инцидентности в виде двумерного массива
+        for(int i = 0; i < matrixToCheck.length; i++) {
+            String lineWithoutSpaces = matrixToCheck[i].replaceAll("\\s+",""); // уберем все пробелы
+            List<Integer> currentRow = new LinkedList<>();
+
+            for(int j = 0; j < lineWithoutSpaces.length(); j++) {
+                if((lineWithoutSpaces.charAt(j) == '0')) {
+                    currentRow.add(0);
+                    continue;
+                } else if(lineWithoutSpaces.charAt(j) == '1') {
+                    currentRow.add(1);
+                    continue;
+                } else if(lineWithoutSpaces.charAt(j) == '-') {
+                    currentRow.add(-1);
+                    i++; // т.к. -1 занимает 2(два) символа
+                    continue;
+                } else if(lineWithoutSpaces.charAt(j) == '2') {
+                    currentRow.add(2);
+                    continue;
+                } else {
+                    // ошибка, любые другие символы недопустимы
+                    errorString = "Ошибка! Символ: " + lineWithoutSpaces.charAt(j) + " недопустим в строке: " + matrixToCheck[i];
+                    return false;
+                }
+            }
+
+
+        }
+
+        return true;
+    }
+
+    private boolean isColumnNumberOk() {
+
+        return false;
+    }
+
 }
